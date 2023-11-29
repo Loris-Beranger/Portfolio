3ae071f37e0e5b2f9014fa1ae772ccf41752b521
Loris-Beranger <lolo.beranger1@gmail.com>
2023-11-29 23:46:25 +0100

    fonctionnal form

--- app/components/sections/contact/ContactSection.tsx
+++ app/components/sections/contact/ContactSection.tsx
@@ -1,11 +1,48 @@
-import React from 'react'
-import styles from './contactSection.module.scss'
+'use client'
 
+import React, { useRef } from 'react'
+import styles from './contactSection.module.scss'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import emailjs from '@emailjs/browser'
+
+type Inputs = {
+  name: string
+  mail: string
+  object: string
+  message: string
+}
 export const ContactSection = () => {
+  const formRef = useRef<HTMLFormElement>(null)
+  const { register, handleSubmit } = useForm<Inputs>()
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data)
+    if (formRef.current) {
+      emailjs
+        .sendForm(
+          'service_s09egjn',
+          'template_mtvo8pq',
+          formRef.current,
+          'aNwpZROYgEo05D2f6'
+        )
+        .then(
+          (result) => {
+            console.log(result.text)
+          },
+          (error) => {
+            console.log(error.text)
+          }
+        )
+    }
+  }
+
   return (
     <section id={'contact'} className={styles.contactContainer}>
       <h2 className={styles.title}>Contact</h2>
-      <form className={styles.formContainer}>
+      <form
+        ref={formRef}
+        className={styles.formContainer}
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <div className={styles.inputContainer}>
           <label className={styles.labelName} htmlFor="name">
             Nom
@@ -14,6 +51,7 @@ export const ContactSection = () => {
             className={styles.inputName}
             type="text"
             placeholder="Beranger"
+            {...register('name')}
           />
         </div>
         <div className={styles.inputContainer}>
@@ -24,6 +62,7 @@ export const ContactSection = () => {
             className={styles.inputMail}
             type="email"
             placeholder="loris.beranger1@gmail.com"
+            {...register('mail')}
           />
         </div>
         <div className={styles.inputContainer}>
@@ -34,13 +73,18 @@ export const ContactSection = () => {
             className={styles.inputObject}
             type="text"
             placeholder="Objet"
+            {...register('object')}
           />
         </div>
         <div className={styles.inputContainer}>
           <label className={styles.labelMessage} htmlFor="message">
             Message
           </label>
-          <textarea className={styles.inputMessage} placeholder="Bonjour..." />
+          <textarea
+            className={styles.inputMessage}
+            placeholder="Bonjour..."
+            {...register('message')}
+          />
         </div>
         <input className={styles.inputSubmit} type="submit" value="Envoyer" />
       </form>
