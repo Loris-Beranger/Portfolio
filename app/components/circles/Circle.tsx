851134a2cb798b59fe926808e75c37c64e136929
Loris-Beranger <lolo.beranger1@gmail.com>
2023-12-05 21:54:12 +0100

    responsive mobile

--- app/components/circles/Circle.tsx
+++ app/components/circles/Circle.tsx
@@ -6,11 +6,12 @@ import { useScroll } from '@/app/hooks/useScroll'
 
 export const Circles = () => {
   const scrollY = useScroll()
-
+  const sectionProjects = document.querySelector('#projects') as HTMLElement
+  const projectSectionPosition = sectionProjects?.offsetTop - 48
   return (
     <div
       className={`${styles.circlesContainer} ${
-        scrollY > 1500 ? styles.bottom : ''
+        scrollY > projectSectionPosition ? styles.bottom : ''
       }`}
     >
       <div className={styles.circle}></div>
